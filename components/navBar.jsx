4671aa9cd19e2262dcdde9858b98538c417d406c
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2023-04-09 06:49:53 +0500

    nav complete, favicon set, template styles removed, app title set

---
+++ components/navBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+
+const NavBar = () => {
+  return (
+    <nav className='container mx-auto flex flex-row justify-between align-middle'>
+      <div className='flex flex-row space-x-14'>
+        <Image className='cursor-pointer' width={109} height={40} src={'/assets/svg/navBarLogo.svg'} alt='taxpal logo' />
+        <div className='flex flex-row my-3 space-x-10 justify-center align-middle'>
+          <p className='text-sm font-normal leading-3 text-slate-700 cursor-pointer'>Features</p>
+          <p className='text-sm font-normal leading-3 text-slate-700 cursor-pointer'>Testimonials</p>
+          <p className='text-sm font-normal leading-3 text-slate-700 cursor-pointer'>Pricing</p>
+        </div>
+      </div>
+      <div className='flex flex-row space-x-8'>
+        <button className='text-sm font-normal leading-3 text-slate-700 cursor-pointer'>Sign in</button>
+        <button className='text-sm font-medium leading-none text-slate-900 cursor-pointer px-4 py-3 border border-slate-900 rounded-3xl hover:text-white hover:bg-blue-600 hover:border-none'>Get started today</button>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+     <Head>
+        <title>Taxpal</title>
+        <meta name="description" content="Accounting made simple for small businesses." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
